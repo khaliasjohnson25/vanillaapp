cf7e72e545b29b6f226f0a06dba381c6bea8fe32
khaliasjohnson25 <115362382+khaliasjohnson25@users.noreply.github.com>
2022-11-18 18:58:40 -0500

    update function

--- vanilla-weatherApp/src/index.js
+++ vanilla-weatherApp/src/index.js
@@ -42,23 +42,24 @@ function displayForecast(response) {
         forecastHTML +
         `
     <div class="col-2">
-      <div class="forecast-date">${formatDay(forecastDay.dt)}</div>
+      <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
       <img
          src="http://openweathermap.org/img/wn/${
            forecastDay.weather[0].icon
          }@2x.png"
         alt="thursday"
-        width="40"
+        width="42"
       />
-      <div class="weather-forecast-temperature">
-        <span class="max-temperature-forecast">${Math.round(
+      <div class="weather-forecast-temperatures">
+        <span class="weather-forecast-temperature-max">${Math.round(
           forecastDay.temp.max
-        )}°</span>
-        <span class="min-temperature-forecast">${Math.round(
+        )}° </span>
+        <span class="weather-forecast-temperature-min">${Math.round(
           forecastDay.temp.min
-        )}°</span>
+        )}° </span>
       </div>
-    </div>`;
+    </div>
+    `;
     }
   });
 
@@ -114,4 +115,4 @@ function handleSubmit(event) {
 let form = document.querySelector("#search-form");
 form.addEventListener("submit", handleSubmit);
 
-search("New York");
+search("New York","Washington DC","Los Angeles");
