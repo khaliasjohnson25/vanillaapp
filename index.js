74b47c097598f04ed73172860205c9be82909035
khaliasjohnson25 <115362382+khaliasjohnson25@users.noreply.github.com>
2022-11-18 20:01:27 -0500

    Update index.js

--- index.js
+++ index.js
@@ -68,8 +68,8 @@ function displayForecast(response) {
 }
 
 function getForecast(coordinates) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
+  let apiKey = "a34tf68cfb143a32002a6d05a5caocaf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query={query}&key={key}&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -92,7 +92,7 @@ function displayTemperature(response) {
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `"http://shecodes-assets.s3.amazonaws.com/api/weather/icons${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 
@@ -100,8 +100,8 @@ function displayTemperature(response) {
 }
 
 function search(city) {
-  let apiKey = "2ff29bed3181c3526c35cc5408037f85";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+  let apiKey = "a34tf68cfb143a32002a6d05a5caocaf";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query={query}&key={key}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
