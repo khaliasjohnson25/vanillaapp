f4e2f650bf3622e42c8135fb848080d67852c2f5
khaliasjohnson25 <115362382+khaliasjohnson25@users.noreply.github.com>
2022-11-19 11:19:27 -0500

    changed city

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function displayForecast(response) {
 
 function getForecast(coordinates) {
   let apiKey = "a34tf68cfb143a32002a6d05a5caocaf";
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${coordinates.longitude}&lat=${coordinates.latitude}&key=${apiKey}&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=-77.0365427&lat=38.8950368&key=a34tf68cfb143a32002a6d05a5caocaf&units=metric`;
   axios.get(apiUrl).then(displayForecast);
 }
 
@@ -96,7 +96,7 @@ function displayTemperature(response) {
 
 function search(city) {
   let apiKey = "a34tf68cfb143a32002a6d05a5caocaf";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=paris&key=a34tf68cfb143a32002a6d05a5caocaf&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=washington&key=a34tf68cfb143a32002a6d05a5caocaf&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
 }
 
